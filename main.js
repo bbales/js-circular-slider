26d0366f535f1c2e4e954ff3405944874daf836e
bbales <ben.bales.dev@gmail.com>
2016-01-31 15:16:38 -0500

    colors and customization, arc abstraction function

--- main.js
+++ main.js
@@ -5,14 +5,20 @@ var mouse = {};
   canvas.width = 400;
   canvas.height = 400;
 
-circles = {
-  first : {
-    rad : 40
-  },
-  second : {
-    rad : 90
-  }
-};
+  circles = {
+    first : {
+      rad : 40,
+      bg : "#1bc6da",
+      fg : "#108646",
+      handleRad : 12
+    },
+    second : {
+      rad : 90,
+      bg : "#a02db8",
+      fg : "#df75a3",
+      handleRad : 15
+    }
+  };
 
   mouse.down = {
     x : 0,
@@ -66,64 +72,67 @@ circles = {
 
   var rad;
 
+  ctx.drawCirc = function(x,y,r,stroke,fill){
+    this.beginPath();
+    this.arc(x,y,r,0,2*Math.PI);
+
+    if(fill !== undefined){
+      this.fillStyle = fill;
+      this.fill();
+    }
+
+    if(stroke !== undefined) this.strokeStyle = stroke;
+    this.stroke();
+  }
+
   function draw(){
     // Clear
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
-    // Origin
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(199,199,2,2);
-
     // Inner circle
-    ctx.beginPath();
-    ctx.arc(canvas.width/2,canvas.height/2,circles.first.rad,0,2*Math.PI);
-    ctx.stroke();
+    ctx.lineWidth = 12;
+    ctx.drawCirc(canvas.width/2,canvas.height/2,circles.first.rad,circles.first.bg)
 
     // Outer circle
-    ctx.beginPath();
-    ctx.arc(canvas.width/2,canvas.height/2,circles.second.rad,0,2*Math.PI);
-    ctx.stroke();
+    ctx.drawCirc(canvas.width/2,canvas.height/2,circles.second.rad,circles.second.bg);
+
+    ctx.lineWidth = 1;
 
     // declarations
     var mag, angle, cx, cy;
+
+    // Draw moving circle and stationary
     if(mouse.down.enabled){
       // Get magnitude from Origin
       mag = Math.sqrt(Math.pow(mouse.down.x - canvas.width/2,2) + Math.pow(mouse.down.y - canvas.height/2,2));
       rad = (mag < circles.first.rad * 1.12 ? circles.first.rad : circles.second.rad);
+
       // Get angle of mouse relative to Origin
       angle = Math.atan((mouse.move.y-canvas.height/2)/(mouse.move.x-canvas.width/2));
+
       // Draw a circle in a fixed radius at the calculated angle
       cx = Math.cos(angle) * rad;
       cy = Math.sin(angle) * rad;
 
-      ctx.beginPath();
-      ctx.arc((mouse.move.x >= canvas.width/2 ? cx : -1*cx)+canvas.width/2,(mouse.move.x >= canvas.width/2 ? cy : -1*cy)+canvas.height/2,10,0,2*Math.PI);
-      ctx.stroke();
-
-      // Draw unaffected circle
+      // Draw the affected circle
       if(rad != circles.first.rad){
-        ctx.beginPath();
-        ctx.arc(circles.first.x,circles.first.y,10,0,2*Math.PI);
-        ctx.stroke();
+        ctx.drawCirc((mouse.move.x >= canvas.width/2 ? cx : -1*cx)+canvas.width/2,(mouse.move.x >= canvas.width/2 ? cy : -1*cy)+canvas.height/2,circles.second.handleRad,circles.second.fg,circles.second.fg);
+        ctx.drawCirc(circles.first.x,circles.first.y,circles.first.handleRad,circles.first.fg,circles.first.fg);
       }else{
-        ctx.beginPath();
-        ctx.arc(circles.second.x,circles.second.y,10,0,2*Math.PI);
-        ctx.stroke();
+        ctx.drawCirc((mouse.move.x >= canvas.width/2 ? cx : -1*cx)+canvas.width/2,(mouse.move.x >= canvas.width/2 ? cy : -1*cy)+canvas.height/2,circles.first.handleRad,circles.first.fg,circles.first.fg);
+        ctx.drawCirc(circles.second.x,circles.second.y,circles.second.handleRad,circles.second.fg,circles.second.fg);
       }
     }
 
+    // Save circle positions
     if(!mouse.down.enabled && !mouse.up.checked){
       mouse.up.checked = true;
       // Save last change
       angle = Math.atan((mouse.move.y-canvas.height/2)/(mouse.move.x-canvas.width/2));
+
       // Draw a circle in a fixed radius at the calculated angle
       cx = Math.cos(angle) * rad;
       cy = Math.sin(angle) * rad;
-
-      // Draw affected circle
-      ctx.beginPath();
-      ctx.arc((mouse.move.x > canvas.width/2 ? cx : -1*cx)+canvas.width/2,(mouse.move.x > canvas.width/2 ? cy : -1*cy)+canvas.height/2,10,0,2*Math.PI);
-      ctx.stroke();
 
       // Save position
       if(rad == circles.first.rad){
@@ -135,13 +144,10 @@ circles = {
       }
     }
 
+    // Draw stationary circles
     if(!mouse.down.enabled && mouse.up.checked){
-        ctx.beginPath();
-        ctx.arc(circles.first.x,circles.first.y,10,0,2*Math.PI);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.arc(circles.second.x,circles.second.y,10,0,2*Math.PI);
-        ctx.stroke();
+        ctx.drawCirc(circles.first.x,circles.first.y,circles.first.handleRad,circles.first.fg,circles.first.fg);
+        ctx.drawCirc(circles.second.x,circles.second.y,circles.second.handleRad,circles.second.fg,circles.second.fg);
     }
   }
 
